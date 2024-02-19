167f2012ca6dff6bfe803dd88dabaccc8e597fd6
somi batool <somibatool44@gmail.com>
2024-02-19 16:20:47 +0500

    add new flies

---
+++ javascript.js
@@ -0,0 +1,25 @@
+// let slideIndex = 0;
+// showTestimonials(slideIndex);
+
+// function showTestimonials(index) {
+//   const testimonials = document.getElementsByClassName("testimonial");
+//   if (index >= testimonials.length) {
+//     slideIndex = 0;
+//   }
+//   if (index < 0) {
+//     slideIndex = testimonials.length - 1;
+//   }
+//   for (let i = 0; i < testimonials.length; i++) {
+//     testimonials[i].classList.remove("active");
+//   }
+//   testimonials[slideIndex].classList.add("active");
+// }
+
+// document.getElementById("prevBtn").addEventListener("click", () => {
+//   showTestimonials(--slideIndex);
+// });
+
+// document.getElementById("nextBtn").addEventListener("click", () => {
+//   showTestimonials(++slideIndex);
+// });
+
